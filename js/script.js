efa3a5b5235fc8a23576147c33ae325f5e0ff7aa
AInsolence <antoniovbak14@gmail.com>
2016-10-05 17:40:06 +0600

    added NY articles api func

--- js/script.js
+++ js/script.js
@@ -14,10 +14,25 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
-    var svAdress = $("#street").val() + ', ' + $("#city").val()
-    $greeting.text('So, you want to live at ' + svAdress + '?')
-    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=1600x1200&location=' + svAdress + ''
-    $body.append("<img class = 'bgimg' src = '" + streetviewUrl + "'>")
+    city = $("#city").val()
+    var svAdress = $("#street").val() + ', ' + city;
+    $greeting.text('So, you want to live at ' + svAdress + '?');
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=1600x1200&location=' + svAdress + '';
+    $body.append("<img class = 'bgimg' src = '" + streetviewUrl + "'>");
+
+    // Built by LucyBot. www.lucybot.com
+
+    var NYurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + "&sort=newest&api-key=e5879d59e6614412a48e5616b09b01a2";
+    $.getJSON(NYurl, function(data){
+        $nytHeaderElem.text("NY Times Article about " + city + ':');
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++ ){
+            var article = articles[i];
+            $nytElem.append("<li class = 'article'>" + "<a href = '" + article.web_url + "'>" + article.headline.main + "</a>" + 
+                "<p>" + article.snippet + "</p>" + "</li>")
+        }
+
+    })
 
     return false;
 
